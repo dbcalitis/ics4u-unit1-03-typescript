18b0b7ea5597bdc4503a476f3e8fd7d388eb0971
Daria Calitis <60710963+dbcalitis@users.noreply.github.com>
2022-09-19 22:06:08 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ switch (food) {
 }
 
 if (foodTime !== 0) {
-  const amountFood = prompt(`How many ${food}(s) are you cooking? (max:3): `)
+  const amountFood = prompt(`How many ${String(food)}(s) are you cooking? (max:3): `)
   const amountFoodInt = parseInt(amountFood)
 
   if (isNaN(amountFoodInt) || amountFoodInt > 3) {
